1c8a809942858480c331d086681a77db50c96836
astalberto <albertoherrera205@hotmail.com>
2023-12-13 16:14:44 -0500

    Primer push

--- Ejemplos/src/paquete1/Ejemplo03.java
+++ Ejemplos/src/paquete1/Ejemplo03.java
@@ -19,17 +19,20 @@ public class Ejemplo03 {
 
         // arreglo de tipo entero
         int[] arreglo1 = new int[5];
-        
+
         // arreglo de tipo entero
         int[] arreglo2;
         arreglo2 = new int[5];
-        
+
         // arreglo de tipo entero
         int[] arreglo3 = {5, 10, 15, 20, 25}; // sin new
-        
+        // La diferecnia entre 1 y 2 es que en 1 se declara el indice en una soloalinea
+        // y la 2 declara el indice en 2 lineas separadas
+        // y la 3 se declara los valores que va a tener el arreglo
+
         // arreglo de doubles - 3 elementos
         double[] arreglo4 = {5.1, 10.2, 15.2}; // sin new
-        
+
         // arreglo de cadenas - 4 elementos
         String[] arreglo5 = {"Loja", "Quito", "Zamora", "Machala"};
     }
--- Ejemplos/src/paquete1/Ejemplo041.java
+++ Ejemplos/src/paquete1/Ejemplo041.java
@@ -9,26 +9,37 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo041 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         //int[] arreglo1 = new int[5];
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
-        // la propiedad .length nos da el numero de elementos que tiene el arreglo al que esta asociado
+        // la propiedad .length nos da el numero de elementos que tiene el 
+        // arreglo al que esta asociado
+        String acumulador = "";
+        int suma = 0;
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            suma = suma + arreglo1[i];
+            acumulador = String.format("%sSubíndice o Índice %d,"
+                    + " con valor %d\n",
+                    acumulador,
+                    i,
+                    arreglo1[i]);
         }
+        acumulador = String.format("%sLa suma total es: %d\n",
+                acumulador,
+                suma);
         
-        
+        System.out.printf("%s", acumulador.toUpperCase());
+
     }
 
 }
